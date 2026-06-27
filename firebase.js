62732ec1f34aabc055a825ab2c9ad0eeb885fd1f
Muhammadhamza1261 <ameeralbaloshi377@gmail.com>
2026-06-27 20:45:34 +0500

    complete

--- firebase.js
+++ firebase.js
@@ -5,13 +5,13 @@ import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword ,  
   signOut } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCfZQbPUqS4_A-9KEjivxc-SClQDBegNU8",
-  authDomain: "assignment-smit-c523a.firebaseapp.com",
-  projectId: "assignment-smit-c523a",
-  storageBucket: "assignment-smit-c523a.firebasestorage.app",
+  apiKey: "<enter your name>",
+  authDomain: "<enter your name>",
+  projectId: "<enter your name>",
+  storageBucket: "<enter your name>",
   messagingSenderId: "715132384694",
-  appId: "1:715132384694:web:96a70e8990a18af5ccbd17",
-  measurementId: "G-KFSHPMQZMR"
+  appId: "<enter your name>",
+  measurementId: "<enter your name>"
 };
 
 // Initialize Firebase
